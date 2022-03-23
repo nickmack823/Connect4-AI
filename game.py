1a117e3cc7990fdc7c8b730d56dd7d915298edf8
MacKinnon, Nicholas Stephen <nicmacki@iu.edu>
2022-03-23 09:54:46 -0400

    Game configuration finished, result recording WIP

--- game.py
+++ game.py
@@ -57,11 +57,9 @@ def showConfigureGameMenu():
     menu = pygame_menu.Menu('Connect4 AI', width, height,
                             theme=pygame_menu.themes.THEME_BLUE)
 
-    def updateGameCount():
+    def updateGameCount(input):
         global gameCount
-        inputBox = menu.get_widget('gameCount')
-        if inputBox is not None:
-            gameCount = inputBox.get_value()
+        gameCount = int(input) if input.isnumeric() else 0
         print(gameCount)
 
     menu.add.text_input('Number of Games: ', textinput_id='gameCount', onchange=updateGameCount)
@@ -69,20 +67,15 @@ def showConfigureGameMenu():
     def updateP1Depth(input):
         global player1depth
         player1depth = int(input) if input.isnumeric() else None
-        print(player1depth)
     def updateP2Depth(input):
         global player2depth
         player2depth = int(input) if input.isnumeric() else None
-        print(player2depth)
     def updateP1TestGames(input):
         global player1TestGamesMC
         player1TestGamesMC = int(input) if input.isnumeric() else None
-        print(player1TestGamesMC)
     def updateP2TestGames(input):
-        print("UPDATE P2 TEST GAMES")
         global player2TestGamesMC
         player2TestGamesMC = int(input) if input.isnumeric() else None
-        print(player2TestGamesMC)
 
     menu.add.text_input('Maximum Depth (P1): ', textinput_id='depthP1', onchange=updateP1Depth).hide()
     menu.add.text_input('Monte Carlo Test Games Per Move (P1): ', textinput_id='testGamesP1',
@@ -102,6 +95,9 @@ def showConfigureGameMenu():
         elif value == 5:
             menu.get_widget('testGamesP1').show()
             menu.get_widget('depthP1').hide()
+        else:
+            menu.get_widget('depthP1').hide()
+            menu.get_widget('testGamesP1').hide()
 
     def updateP2(name, value):
         global player2Selection
@@ -112,6 +108,9 @@ def showConfigureGameMenu():
         elif value == 5:
             menu.get_widget('testGamesP2').show()
             menu.get_widget('depthP2').hide()
+        else:
+            menu.get_widget('depthP2').hide()
+            menu.get_widget('testGamesP2').hide()
 
     menu.add.selector('Player 1 ',
                       [('You', 1), ('Random', 2), ('MiniMax', 3), ('Alpha-Beta', 4), ('Monte Carlo', 5)],
@@ -128,7 +127,7 @@ def showConfigureGameMenu():
 
 def startGames():
     print("Starting Game(s)")
-    if player1Selection is not None and player2Selection is not None:
+    if gameCount >= 1:
         p1Value = None
         p2Value = None
         if player1Selection == 3 or player1Selection == 4:
@@ -151,8 +150,13 @@ def startGames():
 
         p1 = Game.getPlayer(player1Selection, p1Value)
         p2 = Game.getPlayer(player2Selection, p2Value)
-        game = Game(p1, p2)
-        game.playGame()
+
+        games = []
+        for n in range(gameCount):
+            game = Game(p1, p2)
+            game.playGame()
+            games.append(game)
+        printResults(games, gameCount)
 
 
 def showData():
@@ -181,13 +185,57 @@ def drawHoveringPiece(x, color):
     pygame.display.update()
 
 
+def printResults(games, n):
+    totalTimeP1, totalTimeP2, totalMovesConsideredP1, totalMovesConsideredP2 = 0, 0, 0, 0
+    p1 = games[0].player1.label
+    p2 = games[0].player2.label
+    finalResults = {p1: 0, p2: 0, 'Draw': 0}
+    for game in games:
+        if len(games) == 1:
+            game_number = " "
+        else:
+            game_number = " " + str(games.index(game) + 1) + " "
+        print("\nGAME" + game_number + "RESULTS (" + p1 + " vs. " + p2 + ")\n")
+        if p1 == "Minimax" or p1 == "Alpha-Beta":
+            print(p1 + " depth (P1): " + str(game.player1.max_depth))
+        if p2 == "Minimax" or p2 == "Alpha-Beta":
+            print(p2 + " depth (P2): " + str(game.player2.max_depth))
+        if p1 == "Monte Carlo":
+            print("Random games played per possible move (Monte Carlo, P1): " + str(game.player1.test_total))
+        if p2 == "Monte Carlo":
+            print("Random games played per possible move (Monte Carlo, P2): " + str(game.player2.test_total))
+        print("Results: " + str(game.results))
+        print("\nPlayer 1 (" + p1 + ")")
+        print("Total moves considered: " + "{:,}".format(game.moves_considered_p1))
+        print("Average moves considered per game: " + "{:,}".format(game.moves_considered_p1 / n))
+        print("Time spent considering moves: " + str(round(game.time_elapsed_p1, 2)) + " seconds")
+        print("Average time spent per game: " + str(round(game.time_elapsed_p1 / n, 2)) + " seconds")
+        print("\nPlayer 2 (" + p2 + ")")
+        print("Total moves considered: " + "{:,}".format(game.moves_considered_p2))
+        print("Average moves considered per game: " + "{:,}".format(game.moves_considered_p2 / n))
+        print("Time spent considering moves: " + str(round(game.time_elapsed_p2, 2)) + " seconds")
+        print("Average time spent per game: " + str(round(game.time_elapsed_p2 / n, 2)) + " seconds")
+
+        totalTimeP1 += game.time_elapsed_p1
+        totalTimeP2 += game.time_elapsed_p2
+        totalMovesConsideredP1 += game.moves_considered_p1
+        totalMovesConsideredP2 += game.moves_considered_p2
+        finalResults = {p1: finalResults.get(p1) + game.results.get(p1),
+                        p2: finalResults.get(p2) + game.results.get(p2),
+                        'Draw': finalResults.get('Draw') + game.results.get('Draw')}
+
+    print(p1 + " vs. " + p2 + " (" + str(n) + " games)")
+    print("OVERALL RESULTS: " + str(finalResults))
+
+
+
 class Game:
 
     def __init__(self, player1, player2):
         self.move_history = []
         self.player1 = player1
         self.player2 = player2
-        self.total_moves_p1, self.total_moves_p2 = 0, 0
+        self.moves_considered_p1, self.moves_considered_p2 = 0, 0
         self.time_elapsed_p1, self.time_elapsed_p2 = 0, 0
         self.total_time = 0
         self.results = {player1.label: 0, player2.label: 0, 'Draw': 0}
@@ -198,11 +246,12 @@ class Game:
         drawBoard()
         self.player1.moves_considered, self.player2.moves_considered = 0, 0
         print(self.player1.label + " vs. " + self.player2.label)
+        time_start_p1 = None
 
         while not board.game_over:
             move = None
             if board.turn == 1:
-                time_start = time.time()
+                time_start_p1 = time.time() if time_start_p1 is None else time_start_p1
                 # If user playing manually
                 if player1Selection == 1:
                     for event in pygame.event.get():
@@ -213,35 +262,39 @@ class Game:
                             x = event.pos[0]
                             col = int(math.floor(x / SQUARE_SIZE))
                             print(col)
-                            move = self.player1.findMove(self.player1, board.move_history, col)
+                            move = self.player1.findMove(board.move_history, col)
                             print("Move: " + str(move))
                             # Move valid, continue onwards
                             if move is not None:
                                 break
                 # AI as Player 1
                 else:
-                    move = self.player1.findMove(self.player1, board.move_history)
+                    move = self.player1.findMove(board.move_history)
                     print(move)
                 if move is not None:
-                    time_end = time.time()
-                    self.time_elapsed_p1 += time_end - time_start
+                    time_end_p1 = time.time()
+                    print(time_start_p1)
+                    print(time_end_p1)
+                    self.time_elapsed_p1 += time_end_p1 - time_start_p1
+                    print(self.time_elapsed_p1)
                     drawPiece(move, board.getMoveRow(move), RED)
+                    time_start_p1 = None
                     print("Player 1 move: " + str(move))
             if board.turn == 2:
                 print("Player 2 turn")
-                time_start = time.time()
+                time_start_p2 = time.time()
                 move = self.player2.findMove(board.move_history)
-                time_end = time.time()
-                self.time_elapsed_p2 += time_end - time_start
+                time_end_p2 = time.time()
+                self.time_elapsed_p2 += time_end_p2 - time_start_p2
                 print("Player 2 move: " + str(move))
                 drawPiece(move, board.getMoveRow(move), YELLOW)
             if move is not None:
-                print(move)
+                # print(move)
                 board.makeMove(move)
-                board.printBoard()
+                # board.printBoard()
 
-                self.total_moves_p1 += self.player1.moves_considered
-                self.total_moves_p2 += self.player2.moves_considered
+                self.moves_considered_p1 += self.player1.moves_considered
+                self.moves_considered_p2 += self.player2.moves_considered
 
                 if board.winner == 1:
                     print("Player 1 wins.")
@@ -259,77 +312,47 @@ class Game:
             self.results[self.player2.label] += 1
         else:
             self.results["Draw"] += 1
-        self.total_moves_p1 += self.player1.moves_considered
-        self.total_moves_p2 += self.player2.moves_considered
+        self.moves_considered_p1 += self.player1.moves_considered
+        self.moves_considered_p2 += self.player2.moves_considered
 
-    def testSuite(self, n):
-        randPlayer = PlayerRandom()
-        minimaxPlayer = PlayerMM(3, 1)  # Uses depth 3 and heuristic 1 by default
-        alphaBetaPlayer = PlayerAB(3, 1)  # Uses depth 3 and heuristic 1 by default
-        monteCarloPlayer = PlayerMC(100)  # Performs 100 test games by default
-        players = [randPlayer, minimaxPlayer, alphaBetaPlayer, monteCarloPlayer]
-        games = []
-        matchups = []
-
-        # Generate a Game for all possible matchups given the Player instances we have
-        for player in players:
-            # Random doesn't deserve to be player 1
-            if player.label == "Random":
-                continue
-            for otherPlayer in players:
-                # Avoid putting Player against itself
-                if otherPlayer != player:
-                    matchup = player.label + "/" + otherPlayer.label
-                    reversedMatchup = otherPlayer.label + "/" + player.label
-                    # Create Game for matchup if these two Players aren't already set to go against each other
-                    if matchup not in matchups and reversedMatchup not in matchups:
-                        matchups.append(matchup)
-                        game = Game(player, otherPlayer)
-                        games.append(game)
-
-        # Play each game in games n times
-        for iteration in range(0, n):
-            for game in games:
-                game.playGame()
-        self.printResults(games, n)
-
-    def printResults(self, games, n):
-        for game in games:
-            p1 = game.player1.label
-            p2 = game.player2.label
-            if len(games) == 1:
-                game_number = " "
-            else:
-                game_number = " " + str(games.index(game) + 1) + " "
-            print("\nGAME" + game_number + "RESULTS (" + p1 + " vs. " + p2 + ")\n")
-            print(p1 + " vs. " + p2 + " (" + str(n) + " games)")
-            if p1 == "Minimax" or p1 == "Alpha-Beta":
-                print(p1 + " depth: " + str(game.player1.max_depth))
-            if p2 == "Minimax" or p2 == "Alpha-Beta":
-                print(p2 + " depth: " + str(game.player2.max_depth))
-            if p1 == "Monte Carlo":
-                print("Random games played per possible move (Monte Carlo): " + str(game.player1.test_total))
-            elif p2 == "Monte Carlo":
-                print("Random games played per possible move (Monte Carlo): " + str(game.player2.test_total))
-            print("Results: " + str(game.results))
-            print("\nPlayer 1 (" + p1 + ")")
-            print("Total moves considered: " + "{:,}".format(game.total_moves_p1))
-            print("Average moves considered per game: " + "{:,}".format(game.total_moves_p1 / n))
-            print("Time spent considering moves: " + str(round(game.time_elapsed_p1, 2)) + " seconds")
-            print("Average time spent per game: " + str(round(game.time_elapsed_p1 / n, 2)) + " seconds")
-            print("\nPlayer 2 (" + p2 + ")")
-            print("Total moves considered: " + "{:,}".format(game.total_moves_p2))
-            print("Average moves considered per game: " + "{:,}".format(game.total_moves_p2 / n))
-            print("Time spent considering moves: " + str(round(game.time_elapsed_p2, 2)) + " seconds")
-            print("Average time spent per game: " + str(round(game.time_elapsed_p2 / n, 2)) + " seconds")
+    # def testSuite(self, n):
+    #     randPlayer = PlayerRandom()
+    #     minimaxPlayer = PlayerMM(3, 1)  # Uses depth 3 and heuristic 1 by default
+    #     alphaBetaPlayer = PlayerAB(3, 1)  # Uses depth 3 and heuristic 1 by default
+    #     monteCarloPlayer = PlayerMC(100)  # Performs 100 test games by default
+    #     players = [randPlayer, minimaxPlayer, alphaBetaPlayer, monteCarloPlayer]
+    #     games = []
+    #     matchups = []
+    #
+    #     # Generate a Game for all possible matchups given the Player instances we have
+    #     for player in players:
+    #         # Random doesn't deserve to be player 1
+    #         if player.label == "Random":
+    #             continue
+    #         for otherPlayer in players:
+    #             # Avoid putting Player against itself
+    #             if otherPlayer != player:
+    #                 matchup = player.label + "/" + otherPlayer.label
+    #                 reversedMatchup = otherPlayer.label + "/" + player.label
+    #                 # Create Game for matchup if these two Players aren't already set to go against each other
+    #                 if matchup not in matchups and reversedMatchup not in matchups:
+    #                     matchups.append(matchup)
+    #                     game = Game(player, otherPlayer)
+    #                     games.append(game)
+    #
+    #     # Play each game in games n times
+    #     for iteration in range(0, n):
+    #         for game in games:
+    #             game.playGame()
+    #     self.printResults(games, n)
 
     @classmethod
     def getPlayer(cls, selection, parameterValue):
         player = None
         if selection == 1:
-            player = ManualPlayer
+            player = ManualPlayer()
         elif selection == 2:
-            player = PlayerRandom
+            player = PlayerRandom()
         elif selection == 3:
             player = PlayerMM(parameterValue)
         elif selection == 4:
@@ -408,10 +431,3 @@ class Game:
 
 if __name__ == "__main__":
     showMainMenu()
-
-    p1 = ManualPlayer()
-    p2 = PlayerRandom()
-    g = Game(p1, p2)
-
-    # g.drawBoard()
-    # g.playGame()
